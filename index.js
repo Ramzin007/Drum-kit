43d4e7914243102266262ab42b1690f521e61ec8
Muhammed Ramzin P <muhammedramzinp0@gmail.com>
2025-02-20 16:19:27 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+var numberOfDrumButton=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfDrumButton;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {alert("Clicked👌");})
+}
